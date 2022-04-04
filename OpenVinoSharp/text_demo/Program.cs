0c620002cc9416b2756926718491cbc57df0bf14
yanguojin <guojin_yjs@cumt.edu.cn>
2022-04-04 14:12:17 +0800

    添加项目文件。

---
+++ OpenVinoSharp/text_demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace text_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
