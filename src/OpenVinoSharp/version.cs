2909ce228c221578da783a51bc19177545a00a09
yanguojin <guojin_yjs@cumt.edu.cn>
2023-07-26 10:05:36 +0800

    添加ov

--- src/OpenVinoSharp/version.cs
+++ src/OpenVinoSharp/version.cs
@@ -63,36 +63,7 @@ namespace OpenVinoSharp
         public ulong size;
     }
 
-    /// <summary>
-    /// Global functions under ov namespace
-    /// </summary>
-    public static partial class ov
-    {
-        /// <summary>
-        /// Get version of OpenVINO.
-        /// </summary>
-        /// <returns>Version of OpenVINO</returns>
-        public static Version get_openvino_version()
-        {
-            int l = Marshal.SizeOf(typeof(Version));
-            IntPtr ptr = Marshal.AllocHGlobal(l);
-            ExceptionStatus status = (ExceptionStatus)NativeMethods.ov_get_openvino_version(ptr);
-            if (status != 0)
-            {
-                System.Diagnostics.Debug.WriteLine("ov get_openvino_version() error!");
-                return new Version();
-            }
-            var temp = Marshal.PtrToStructure(ptr, typeof(Version));
-            Version version = (Version)temp;
-            status = (ExceptionStatus)NativeMethods.ov_version_free(ptr);
-            if (status != 0)
-            {
-                System.Diagnostics.Debug.WriteLine("ov get_openvino_version() error!");
-                return new Version();
-            }
-            return version;
-        }
-    };
+
 
 
 
