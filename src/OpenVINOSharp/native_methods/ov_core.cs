fb3b6ca16b1b0137bec5c6ba3c847c8202b1bdf5
yanguojin <guojin_yjs@cumt.edu.cn>
2023-08-02 18:42:42 +0800

    Modify the method return value in ov_model to ExceptionStatus

--- src/OpenVINOSharp/native_methods/ov_core.cs
+++ src/OpenVINOSharp/native_methods/ov_core.cs
@@ -12,28 +12,28 @@ namespace OpenVinoSharp
         
 
         [DllImport(dll_extern, EntryPoint = "ov_get_openvino_version", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        public extern static int ov_get_openvino_version(IntPtr version);
+        public extern static ExceptionStatus ov_get_openvino_version(IntPtr version);
 
         [DllImport(dll_extern, EntryPoint = "ov_version_free", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        public extern static int ov_version_free(IntPtr version);
+        public extern static ExceptionStatus ov_version_free(IntPtr version);
         [DllImport(dll_extern, EntryPoint = "ov_core_create", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        public extern static int ov_core_create(ref IntPtr core);
+        public extern static ExceptionStatus ov_core_create(ref IntPtr core);
         [DllImport(dll_extern, EntryPoint = "ov_core_create_with_config", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        public extern static int ov_core_create_with_config(string xml_config_file, ref IntPtr core);
+        public extern static ExceptionStatus ov_core_create_with_config(string xml_config_file, ref IntPtr core);
         [DllImport(dll_extern, EntryPoint = "ov_core_free", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        public extern static int ov_core_free(IntPtr core);
+        public extern static void ov_core_free(IntPtr core);
         [DllImport(dll_extern, EntryPoint = "ov_core_read_model_unicode", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        public extern static int ov_core_read_model_unicode(IntPtr core, string model_path, string bin_path, ref IntPtr model);
+        public extern static ExceptionStatus ov_core_read_model_unicode(IntPtr core, string model_path, string bin_path, ref IntPtr model);
         [DllImport(dll_extern, EntryPoint = "ov_core_read_model", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        public extern static int ov_core_read_model(IntPtr core, ref char model_path, ref char bin_path, ref IntPtr model);
+        public extern static ExceptionStatus ov_core_read_model(IntPtr core, ref char model_path, ref char bin_path, ref IntPtr model);
 
         [DllImport(dll_extern, EntryPoint = "ov_core_read_model_from_memory", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        public extern static int ov_core_read_model_from_memory(IntPtr core, string model_path, IntPtr weights, ref IntPtr model);
+        public extern static ExceptionStatus ov_core_read_model_from_memory(IntPtr core, string model_path, IntPtr weights, ref IntPtr model);
         [DllImport(dll_extern, EntryPoint = "ov_core_compile_model", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        public extern static int ov_core_compile_model(IntPtr core, IntPtr model, ref sbyte device_name, ulong property_args_size, ref IntPtr compiled_model);
+        public extern static ExceptionStatus ov_core_compile_model(IntPtr core, IntPtr model, ref sbyte device_name, ulong property_args_size, ref IntPtr compiled_model);
         [DllImport(dll_extern, EntryPoint = "ov_core_get_versions_by_device_name", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        public extern static int ov_core_get_versions_by_device_name(IntPtr core, ref sbyte device_name, IntPtr versions);
+        public extern static ExceptionStatus ov_core_get_versions_by_device_name(IntPtr core, ref sbyte device_name, IntPtr versions);
         [DllImport(dll_extern, EntryPoint = "ov_core_versions_free", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        public extern static int ov_core_versions_free(IntPtr versions);
+        public extern static void ov_core_versions_free(IntPtr versions);
     }
 }
