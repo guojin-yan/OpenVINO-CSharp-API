17cabc3967655db6fb05e541270e2d770758ddd4
yanguojin <guojin_yjs@cumt.edu.cn>
2023-08-09 22:05:29 +0800

    add base.cs

---
+++ src/OpenVINOSharp/base.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// OpenVINO wrapper for .NET. 
+/// This is the basic namespace of OpenVINO in Cshrp, 
+/// and all classes and methods are within this method.
+/// </summary>
+namespace OpenVINOSharp
+{
+}
