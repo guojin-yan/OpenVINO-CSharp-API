52019af58497fc175a2dafe7895a27fb79f2a3ec
国进 颜 <guojin_yjs@cumt.edu.cn>
2023-08-14 20:48:16 +0800

    Modify the format of file content

--- src/OpenVINOSharp/native_methods/ov_node.cs
+++ src/OpenVINOSharp/native_methods/ov_node.cs
@@ -9,32 +9,81 @@ namespace OpenVinoSharp
 {
     public partial class NativeMethods
     {
+        /// <summary>
+        /// Get the shape of port object.
+        /// </summary>
+        /// <param name="port">A pointer to ov_output_const_port_t.</param>
+        /// <param name="tensor_shape">tensor shape.</param>
+        /// <returns>Status code of the operation: OK(0) for success.</returns>
+        [DllImport(dll_extern, EntryPoint = "ov_const_port_get_shape", 
+            CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static ExceptionStatus ov_const_port_get_shape(
+            IntPtr port, 
+            IntPtr tensor_shape);
 
-        [DllImport(dll_extern, EntryPoint = "ov_const_port_get_shape", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        public extern static int ov_const_port_get_shape(IntPtr port, IntPtr tensor_shape);
+        /// <summary>
+        /// Get the shape of port object.
+        /// </summary>
+        /// <param name="port">A pointer to ov_output_port_t.</param>
+        /// <param name="tensor_shape">A pointer to the tensor name.</param>
+        /// <returns>Status code of the operation: OK(0) for success.</returns>
+        [DllImport(dll_extern, EntryPoint = "ov_const_port_get_shape",
+            CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static ExceptionStatus ov_port_get_shape(
+            IntPtr port, 
+            IntPtr tensor_shape);
+        /// <summary>
+        /// Get the tensor name of port.
+        /// </summary>
+        /// <param name="port">A pointer to the ov_output_const_port_t.</param>
+        /// <param name="tensor_name">A pointer to the tensor name.</param>
+        /// <returns>Status code of the operation: OK(0) for success.</returns>
+        [DllImport(dll_extern, EntryPoint = "ov_port_get_any_name", 
+            CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static ExceptionStatus ov_port_get_any_name(
+            IntPtr port, 
+            ref IntPtr tensor_name);
 
-        [DllImport(dll_extern, EntryPoint = "ov_const_port_get_shape", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        /// <summary>
+        /// Get the partial shape of port.
+        /// </summary>
+        /// <param name="port">A pointer to the ov_output_const_port_t.</param>
+        /// <param name="partial_shape">Partial shape.</param>
+        /// <returns>Status code of the operation: OK(0) for success.</returns>
+        [DllImport(dll_extern, EntryPoint = "ov_port_get_partial_shape",
+            CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static ExceptionStatus ov_port_get_partial_shape(
+            IntPtr port, 
+            IntPtr partial_shape);
 
-        public extern static int ov_port_get_shape(IntPtr port, IntPtr tensor_shape);
+        /// <summary>
+        /// Get the tensor type of port.
+        /// </summary>
+        /// <param name="port">A pointer to the ov_output_const_port_t.</param>
+        /// <param name="tensor_type">tensor type.</param>
+        /// <returns>Status code of the operation: OK(0) for success.</returns>
+        [DllImport(dll_extern, EntryPoint = "ov_port_get_element_type", 
+            CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static ExceptionStatus ov_port_get_element_type(
+            IntPtr port, 
+            ref uint tensor_type);
 
-        [DllImport(dll_extern, EntryPoint = "ov_port_get_any_name", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        /// <summary>
+        /// free port object
+        /// </summary>
+        /// <param name="port">The pointer to the instance of the ov_output_port_t to free.</param>
+        [DllImport(dll_extern, EntryPoint = "ov_output_port_free", 
+            CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void ov_output_port_free(
+            IntPtr port);
 
-        public extern static int ov_port_get_any_name(IntPtr port, ref IntPtr tensor_name);
-
-        [DllImport(dll_extern, EntryPoint = "ov_port_get_partial_shape", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-
-        public extern static int ov_port_get_partial_shape(IntPtr port, IntPtr partial_shape);
-
-        [DllImport(dll_extern, EntryPoint = "ov_port_get_element_type", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-
-        public extern static int ov_port_get_element_type(IntPtr port, ref uint tensor_type);
-
-        [DllImport(dll_extern, EntryPoint = "ov_output_port_free", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-
-        public extern static int ov_output_port_free(IntPtr port);
-
-        [DllImport(dll_extern, EntryPoint = "ov_output_const_port_free", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-
-        public extern static int ov_output_const_port_free(IntPtr port);
+        /// <summary>
+        /// free const port
+        /// </summary>
+        /// <param name="port">The pointer to the instance of the ov_output_const_port_t to free.</param>
+        [DllImport(dll_extern, EntryPoint = "ov_output_const_port_free", 
+            CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void ov_output_const_port_free(
+            IntPtr port);
     }
 }
