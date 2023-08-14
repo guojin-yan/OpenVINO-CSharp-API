649cb69efe54f4073d64eb9e3b48973e8d74cbaf
国进 颜 <guojin_yjs@cumt.edu.cn>
2023-08-14 20:46:56 +0800

    add ov_partial_shape file

--- src/OpenVinoSharp/native_methods/ov_partial_shape.cs
+++ src/OpenVinoSharp/native_methods/ov_partial_shape.cs
@@ -10,34 +10,6 @@ namespace OpenVinoSharp
 
     public partial class NativeMethods
     {
-        /// <summary>
-        /// It represents a shape that may be partially or totally dynamic.
-        /// </summary>
-        /// <remarks>
-        /// <para>Dynamic rank. (Informal notation: `?`)</para>
-        /// <para>Static rank, but dynamic dimensions on some or all axes.
-        /// (Informal notation examples: `{1,2,?,4}`, `{?,?,?}`)</para>
-        /// <para> Static rank, and static dimensions on all axes.
-        /// (Informal notation examples: `{1,2,3,4}`, `{6}`, `{}`)</para>
-        /// </remarks>
-        public struct ov_partial_shape
-        {
-            /// <summary>
-            /// The rank
-            /// </summary>
-            public ov_rank rank;
-            /// <summary>
-            /// The dimension
-            /// </summary>
-            public IntPtr dims = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(ov_dimension)));
-            /// <summary>
-            /// Default Constructor
-            /// </summary>
-            public ov_partial_shape() 
-            {
-                rank = new ov_rank();
-                dims = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(ov_dimension)));
-            }
-        }
+
     }
 }
