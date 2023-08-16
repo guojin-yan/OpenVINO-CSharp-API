56c9c4fb1336e455cfd13f1fa4880a0799f0db97
yanguojin <guojin_yjs@cumt.edu.cn>
2023-08-16 15:18:36 +0800

    add namespace comments.

--- src/OpenVINOSharp/base.cs
+++ src/OpenVINOSharp/base.cs
@@ -4,31 +4,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/// <summary>
-/// OpenVINO wrapper for .NET. 
-/// This is the basic namespace of OpenVINO in Cshrp, 
-/// and all classes and methods are within this method.
-/// </summary>
-namespace OpenVINOSharp
+
+namespace OpenVinoSharp
 {
+
     /// <summary>
     /// OpenVINO wrapper for .NET. 
-    /// Define elements in OpenVINO.
+    /// This is the basic namespace of OpenVINO in Cshrp, 
+    /// and all classes and methods are within this method.
+    /// <c>OpenVinoSharp</c>.
     /// </summary>
-    namespace element { }
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    class NamespaceDoc
+    {
+    }
 
-    /// <summary>
-    /// Mainly defined the data processing methods in OpenVINO.
-    /// </summary>
-    namespace preprocess { }
-
-    /// <summary>
-    /// Processing methods for main common models.
-    /// </summary>
-    namespace model {
+    namespace element {
         /// <summary>
-        /// The processing methods of the main Yolov8 model.
+        /// OpenVINO wrapper for .NET. 
+        /// Define elements in OpenVINO.
+        /// <c>OpenVinoSharp.element</c>.
         /// </summary>
-        namespace Yolov8 { }
+        [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+        class NamespaceDoc
+        {
+        }
+    }
+
+
+    namespace preprocess {
+        /// <summary>
+        /// Mainly defined the data processing methods in OpenVINO.
+        /// <c>OpenVinoSharp.preprocess</c>.
+        /// </summary>
+        [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+        class NamespaceDoc
+        {
+        }
+    }
+
+
+    namespace model
+    {
+        /// <summary>
+        /// Processing methods for main common models.
+        /// <c>OpenVinoSharp.model</c>.
+        /// </summary>
+        [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+        class NamespaceDoc
+        {
+        }
+        namespace Yolov8 {
+            /// <summary>
+            /// The processing methods of the main Yolov8 model.
+            /// <c>OpenVinoSharp.model.Yolov8</c>.
+            /// </summary>
+            [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+            class NamespaceDoc
+            {
+            }
+        }
     }
 }
+
+
