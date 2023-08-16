0a420680029fc690406a802f0731b1656587295f
国进 颜 <guojin_yjs@cumt.edu.cn>
2023-08-16 09:14:38 +0800

    Add comments for the Layout class

--- src/OpenVINOSharp/core/layout.cs
+++ src/OpenVINOSharp/core/layout.cs
@@ -7,10 +7,50 @@ using System.Threading.Tasks;
 
 namespace OpenVinoSharp.core
 {
+    /// <summary>
+    /// ov::Layout represents the text information of tensor's dimensions/axes. E.g. layout `NCHW` means that 4D
+    /// tensor `{-1, 3, 480, 640}` will have: 
+    /// - 0: `N = -1`: batch dimension is dynamic 
+    /// - 1: `C = 3`: number of channels is '3' 
+    /// - 2: `H = 480`: image height is 480 
+    /// - 3: `W = 640`: image width is 640 
+    /// </summary>
+    /// <example>
+    /// `ov::Layout` can be specified for:
+    /// - Preprocessing purposes. E.g.
+    ///    - To apply normalization (means/scales) it is usually required to set 'C' dimension in a layout.
+    ///    - To resize the image to specified width/height it is needed to set 'H' and 'W' dimensions in a layout
+    ///    - To transpose image - source and target layout can be set (see
+    ///    `ov::preprocess::PreProcessSteps::convert_layout`)
+    /// - To set/get model's batch (see `ov::get_batch`/`ov::set_batch') it is required in general to specify 'N' dimension
+    /// in layout for appropriate inputs
+    /// </example>
     public class Layout
     {
+        /// <summary>
+        /// [private]Layout class pointer.
+        /// </summary>
         private IntPtr m_ptr = IntPtr.Zero;
+        /// <summary>
+        /// [public]Layout class pointer.
+        /// </summary>
         public IntPtr Ptr { get { return m_ptr; } set { m_ptr = value; } }
+
+        /// <summary>
+        /// Constructs a Layout with static or dynamic layout information based on string representation.
+        /// </summary>
+        /// <param name="layout_desc">
+        /// The string used to construct Layout from.
+        /// The string representation can be in the following form:
+        /// - can define order and meaning for dimensions "NCHW"
+        /// - partial layout specialization:
+        ///   - "NC?" defines 3 dimensional layout, first two NC, 3rd one is not defined
+        ///   - "N...C" defines layout with dynamic rank where 1st dimension is N, last one is C
+        ///   - "NC..." defines layout with dynamic rank where first two are NC, others are not
+        ///   defined
+        /// - only order of dimensions "adbc" (0312)
+        /// - Advanced syntax can be used for multi-character names like "[N,C,H,W,...,CustomName]"
+        /// </param>
         public Layout(string layout_desc)
         {
             sbyte[] c_layout_desc = (sbyte[])((Array)System.Text.Encoding.Default.GetBytes(layout_desc));
@@ -20,7 +60,18 @@ namespace OpenVinoSharp.core
                 System.Diagnostics.Debug.WriteLine("Layout init error : {0}!", status.ToString());
             }
         }
-        ~Layout() { dispose(); }
+
+        /// <summary>
+        /// Default deconstruction
+        /// </summary>
+        ~Layout()
+        {
+            dispose(); 
+        }
+
+        /// <summary>
+        /// Release unmanaged resources.
+        /// </summary>
         public void dispose()
         {
             if (m_ptr == IntPtr.Zero)
@@ -31,6 +82,10 @@ namespace OpenVinoSharp.core
             m_ptr = IntPtr.Zero;
         }
 
+        /// <summary>
+        ///  String representation of Layout.
+        /// </summary>
+        /// <returns>String representation of Layout.</returns>
         public string to_string()
         {
             return NativeMethods.ov_layout_to_string(m_ptr);
--- src/OpenVINOSharp/native_methods/ov_layout.cs
+++ src/OpenVINOSharp/native_methods/ov_layout.cs
@@ -9,15 +9,33 @@ namespace OpenVinoSharp
 {
     public partial class NativeMethods
     {
+        /// <summary>
+        /// Create a layout object.
+        /// </summary>
+        /// <param name="layout_desc">The description of layout.</param>
+        /// <param name="layout">The layout input pointer.</param>
+        /// <returns>a status code, return OK if successful</returns>
+        [DllImport(dll_extern, EntryPoint = "ov_layout_create", 
+            CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static int ov_layout_create(
+            ref sbyte layout_desc, 
+            ref IntPtr layout);
 
+        /// <summary>
+        /// Free layout object.
+        /// </summary>
+        /// <param name="layout">The pointer of layout.</param>
+        [DllImport(dll_extern, EntryPoint = "ov_layout_free", 
+            CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void ov_layout_free(IntPtr layout);
 
-        [DllImport(dll_extern, EntryPoint = "ov_layout_create", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        public extern static int ov_layout_create(ref sbyte layout_desc, ref IntPtr layout);
-
-        [DllImport(dll_extern, EntryPoint = "ov_layout_free", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        public extern static int ov_layout_free(IntPtr layout);
-
-        [DllImport(dll_extern, EntryPoint = "ov_layout_to_string", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        /// <summary>
+        /// Convert layout object to a readable string.
+        /// </summary>
+        /// <param name="layout">layout will be converted.</param>
+        /// <returns>string that describes the layout content.</returns>
+        [DllImport(dll_extern, EntryPoint = "ov_layout_to_string", 
+            CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
         public extern static string ov_layout_to_string(IntPtr layout);
     }
 }
