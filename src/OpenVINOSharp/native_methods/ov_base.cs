2bde171627595f375475c0453d331db89c46049e
国进 颜 <guojin_yjs@cumt.edu.cn>
2023-08-16 09:07:20 +0800

    add comments

--- src/OpenVINOSharp/native_methods/ov_base.cs
+++ src/OpenVINOSharp/native_methods/ov_base.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace OpenVinoSharp
 {
+    /// <summary>
+    /// Introducing C API.
+    /// </summary>
     public partial class NativeMethods
     {
         private const string dll_extern = "./openvino2023.0/openvino_c.dll";
-
-
     }
 }
