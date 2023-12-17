6c1224e63de0f70f91d7a3f404a16802a8376e9d
yanguojin <guojin_yjs@cumt.edu.cn>
2023-12-17 20:45:57 +0800

    Add BenchmarkTests test.

---
+++ tests/csharp_api_extensions_unit_tests/benchmark_app/BenchmarkTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenVinoSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenVinoSharp.Extensions.Tests
+{
+    [TestClass()]
+    public class BenchmarkTests
+    {
+        [TestMethod()]
+        public void sync_benchmark_test()
+        {
+            Benchmark.sync_benchmark("..\\..\\..\\..\\..\\model\\yolov8\\yolov8s-cls.xml");
+        }
+    }
+}
