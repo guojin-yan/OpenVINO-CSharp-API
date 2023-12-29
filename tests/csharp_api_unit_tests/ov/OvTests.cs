93d35efddc2e4299964f3f0ccdfdf9d8949109fd
yanguojin <guojin_yjs@cumt.edu.cn>
2023-12-29 22:17:29 +0800

    Add OvTests class.

---
+++ tests/csharp_api_unit_tests/ov/OvTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenVinoSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenVinoSharp.Tests
+{
+    [TestClass()]
+    public class OvTests : OVBaseTest
+    {
+        [TestMethod()]
+        public void get_openvino_version_test()
+        {
+            Version version = Ov.get_openvino_version();
+            Assert.IsNotNull(version);
+        }
+
+        [TestMethod()]
+        public void content_from_file_test()
+        {
+            byte[] data = Ov.content_from_file(get_model_bin_file_name());
+            Assert.IsTrue(data.Length>0);
+        }
+    }
+}
