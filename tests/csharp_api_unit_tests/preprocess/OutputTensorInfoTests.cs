129f55810cd8a3261e041a59d4b0eb758581174e
yanguojin <guojin_yjs@cumt.edu.cn>
2023-12-30 17:52:35 +0800

    Add OutputTensorInfoTests.

---
+++ tests/csharp_api_unit_tests/preprocess/OutputTensorInfoTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenVinoSharp.preprocess;
+using OpenVinoSharp.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenVinoSharp.preprocess.Tests
+{
+    [TestClass()]
+    public class OutputTensorInfoTests : OVBaseTest
+    {
+        [TestMethod()]
+        public void OutputTensorInfo_test()
+        {
+        }
+
+        [TestMethod()]
+        public void Dispose_test()
+        {
+        }
+
+        [TestMethod()]
+        public void set_element_type_test()
+        {
+            var core = new Core();
+            Model model = core.read_model(get_model_xml_file_name());
+            Assert.IsTrue(model.Ptr != IntPtr.Zero);
+            PrePostProcessor processor = new PrePostProcessor(model);
+            OutputInfo output = processor.output();
+            Assert.IsNotNull(output);
+            OutputTensorInfo output_tensor = output.tensor();
+            Assert.IsNotNull(output_tensor);
+            output_tensor.set_element_type(new OvType(ElementType.F32));
+            output.Dispose();
+            processor.Dispose();
+            model.Dispose();
+        }
+    }
+}
